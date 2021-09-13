8ad8f908ce32c2cf1a36d95db875d9089df3b956
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2021-09-12 18:25:20 -0700

    WIP: Embeds

--- app/Listeners/LibraryNew.php
+++ app/Listeners/LibraryNew.php
@@ -34,11 +34,11 @@ class LibraryNew implements ShouldQueue
 
         $this->request->post(config('services.discord.webhook_url'), [
             'content' => sprintf(
-                'New %s added to %s: **%s**',
+                'New %s added to %s',
                 Str::of($event->payload->Metadata->librarySectionTitle)->lower()->singular(),
                 $event->payload->Server->title,
-                $event->payload->Metadata->title
             ),
+            'embeds' => $this->embeds($event->payload),
         ]);
     }
 
@@ -46,5 +46,61 @@ class LibraryNew implements ShouldQueue
     public function middleware(): array
     {
         return [new RateLimited('discord-webhooks')];
+    }
+
+    /** Get the embeds for the received PLEX event. */
+    private function embeds(object $event): array
+    {
+        return match ($event->Metadata->librarySectionType) {
+            'movie' => [
+                [
+                    'title' => $event->Metadata->title,
+                    'description' => $event->Metadata->tagline,
+                    'fields' => [
+                        $this->inlineField('Year', '1986'),
+                        $this->inlineField('Runtime', '1:23:45'),
+                        $this->inlineField('Rating', 'PG-13'),
+                    ],
+                ],
+            ],
+            'show' => [
+                'title' => $event->Metadata->title,
+                'description' => $event->Metadata->title,
+                'fields' => [
+                    [
+                        $this->inlineField('Season', '04'),
+                        $this->inlineField('Episode', '12'),
+                    ],
+                ],
+            ],
+            'music' => [
+                'title' => $event->Metadata->title,
+                'description' => $event->Metadata->title,
+                'fields' => [
+                    [
+                        $this->inlineField('Year', '1986'),
+                    ],
+                ],
+            ],
+            default => [
+                'title' => $event->Metadata->title,
+            ]
+        };
+    }
+
+    /** Get a field array. */
+    private function field(string $name, string $value, bool $inline = false): array
+    {
+        return [
+            'name' => $name,
+            'value' => $value,
+            'inline' => $inline,
+        ];
+    }
+
+    /** get an inline field array. */
+    private function inlineField(string $name, string $value): array
+    {
+        return $this->field($name, $value, true);
     }
 }
