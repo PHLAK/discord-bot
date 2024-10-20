e67f873f882443b2bac00e015b3da1e77687434d
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2024-10-19 23:35:11 -0700

    Added index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<meta charset="utf-8">
+<meta name="description" content="{{ config('meta_description') }}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>PHLAKNET Discord Bot</title>
+
+<h1>PHLAKNET Discord Bot</h1>
