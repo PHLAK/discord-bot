fe878410d57c257ddab64d83b0529f80afc99153
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2024-10-20 00:53:11 -0700

    Inject the public disk into LibraryNew

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,22 @@
 
 namespace App\Providers;
 
+use App\Listeners\LibraryNew;
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->app->when(LibraryNew::class)->needs(Filesystem::class)->give(
+            fn (Application $app): Filesystem => Storage::disk('public')
+        );
+    }
 
     public function boot(): void
     {
