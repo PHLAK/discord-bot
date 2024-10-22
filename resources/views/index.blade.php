5b651c827816fe171eae09aff6ab56649b123bcf
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2024-10-22 10:18:19 -0700

    Updated landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,20 @@
 <meta name="description" content="PHLAKNET Discord Bot">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+
 <title>PHLAKNET Discord Bot</title>
 
-<h1>PHLAKNET Discord Bot</h1>
+<header>
+    <h1>PHLAKNET Discord Bot</h1>
+</header>
+
+<main>
+    <p align="center">
+        <a href="https://github.com/PHLAK/discord-bot">https://github.com/PHLAK/discord-bot</a>
+    </p>
+</main>
+
+<footer>
+    Made by <a href="https://www.chriskankiewicz.com">Chris Kankiewicz</a>
+</footer>
