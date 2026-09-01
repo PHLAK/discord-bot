34fb5602244ed22ec0d7cad365dd5785d567b552
Chris Kankiewicz <Chris@Kankiewicz.com>
2026-09-01 10:59:08 -0700

    Added Laravel Pulse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,14 @@
 namespace App\Providers;
 
 use App\Listeners\LibraryNew;
+use App\Models\User;
+use Carbon\CarbonImmutable;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
@@ -24,12 +28,18 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Date::use(CarbonImmutable::class);
+
         Event::listen(function (SocialiteWasCalled $event) {
             $event->extendSocialite('pocketid', PocketID\Provider::class);
         });
 
         RateLimiter::for('discord-webhooks', function () {
             return Limit::perMinute(60);
+        });
+
+        Gate::define('viewPulse', function (User $user) {
+            return in_array($user->email, ['Chris@Kankiewicz.com']);
         });
     }
 }
